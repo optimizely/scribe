6749be366a8a6229df35ae004ab4f8a939cbe4c6
Tom Genoni <tom.genoni@optimizely.com>
2015-03-26 15:39:04 -0700

    Adding watch.

---
+++ content/index.md
@@ -0,0 +1,33 @@
+---
+title: Bananas
+foobar: This is something else
+---
+
+## Section Title
+
+{% include "introduction.html"
+  {
+    foo: {
+      title: 'tacos',
+      subject: 'horchata'
+    }
+  }
+%}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed tempora similique blanditiis reprehenderit.
+
+## Another Section
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident hic error, accusantium quod rem, quam, recusandae eligendi amet odit at non saepe. Nisi facilis aliquid eveniet itaque vel expedita. Beatae.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident hic error, accusantium quod rem, quam, recusandae
+eligendi amet odit at non saepe. Nisi facilis aliquid eveniet itaque vel expedita. Beatae.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident hic error, accusantium quod rem, quam, recusandae eligendi amet odit at non saepe. Nisi facilis aliquid eveniet itaque vel expedita. Beatae.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident hic error, accusantium quod rem, quam, recusandae eligendi amet odit at non saepe. Nisi facilis aliquid eveniet itaque vel expedita. Beatae.
+
+- adf
+- asdfadf
+- asdlfkjasdfl
+- ljasdflkaj
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp        = require('gulp')
     frontMatter = require('gulp-front-matter'),
     md          = require('gulp-remarkable');
     swig        = require('gulp-swig'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
     toc         = require('gulp-toc'),
     replace     = require('gulp-replace'),
     wrap        = require('gulp-wrap'),
@@ -10,13 +12,14 @@ var gulp        = require('gulp')
     opts        = {
       setup: function(swig) {
         swig.setDefaults({
+          cache: false,
           loader: swig.loaders.fs(__dirname + '/includes/') // Set partial path root.
         });
       }
 };
 
 gulp.task('build', function() {
-  gulp.src('content/**/*.html')
+  gulp.src('content/**/*.md')
     .pipe(frontMatter({
       property: 'data',
       html: true
@@ -31,11 +34,13 @@ gulp.task('build', function() {
       src: 'templates/template.html'
     }))
     .pipe(toc({
+      // Overrides the default method of building IDs in the content.
       header: '<h<%= level %> id="<%= anchor %>"><%= header %></h<%= level %>>',
       tocMax: 2,
       anchorMax: 2
     }))
     .pipe(gulp.dest('./build/'))
+    .pipe(reload({stream:true}));
 });
 
 gulp.task('assets', function() {
@@ -43,8 +48,24 @@ gulp.task('assets', function() {
     .pipe(gulp.dest('build/assets'));
 });
 
+gulp.task('browser-sync', function() {
+  browserSync({
+    reloadDelay: 300,
+    notify: {
+        styles: [ "position:fixed;top:5px;right:5px;width:10px;height:10px;background:#c82144;border-radius:50%;overflow:hidden;color:#c82144;z-index:99999" ]
+    },
+    server: {
+      baseDir: [__dirname] + '/build/',
+    }
+  });
+});
+
 gulp.task('clean', function (cb) {
   del('./build/', cb);
 });
 
-gulp.task('default', ['build', 'assets']);
+gulp.task('watch', function() {
+    gulp.watch('content/**/*.md', ['build', 'assets']);
+});
+
+gulp.task('default', ['build', 'assets', 'browser-sync', 'watch']);
