d04a2bcb0b4e6fc50c8d0908b9ddd50eea321349
Tom Genoni <tom.genoni@optimizely.com>
2015-04-22 18:47:36 -0700

    Fixing things up.

--- assets/js/stickyTOC.js
+++ assets/js/stickyTOC.js
@@ -4,10 +4,24 @@ $(document).ready(function(){
 
   $(window).scroll(function() {
     var s = $(window).scrollTop();
+    var tocOffset = ($(".ebook-toc-wrap").offset()).top;
+    var tocWrapHeight = $(".ebook-toc-wrap").height();
+    var tocHeight = $(".ebook-toc").height();
+
     if ( s > offset.top - 40) {
-      $("body").addClass("toc-sticky");
+      $("body").addClass("toc-stick-top");
     } else {
-      $("body").removeClass("toc-sticky");
+      $("body").removeClass("toc-stick-top");
+    }
+
+    console.log(s);
+
+    if ( tocOffset + tocWrapHeight - tocHeight - 80 < s ) {
+      var tocBottomOffset = ($(".ebook-toc").offset()).top;
+      $(".ebook-toc").css({
+        position: "absolute",
+        top: "3721px"
+      })
     }
   });
 
