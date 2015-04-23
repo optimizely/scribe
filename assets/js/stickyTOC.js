909ee958c100fcad2b659a69a08ed33c149f3706
Tom Genoni <tom.genoni@optimizely.com>
2015-04-22 18:51:10 -0700

    Waiting to refactor.

--- assets/js/stickyTOC.js
+++ assets/js/stickyTOC.js
@@ -1,28 +1,29 @@
 $(document).ready(function(){
 
-  var offset = $(".ebook-toc").offset();
+  // var offset = $(".ebook-toc").offset();
 
-  $(window).scroll(function() {
-    var s = $(window).scrollTop();
-    var tocOffset = ($(".ebook-toc-wrap").offset()).top;
-    var tocWrapHeight = $(".ebook-toc-wrap").height();
-    var tocHeight = $(".ebook-toc").height();
+  // $(window).scroll(function() {
+  //   var s = $(window).scrollTop();
 
-    if ( s > offset.top - 40) {
-      $("body").addClass("toc-stick-top");
-    } else {
-      $("body").removeClass("toc-stick-top");
-    }
+  //   var tocOffset = ($(".ebook-toc-wrap").offset()).top;
+  //   var tocWrapHeight = $(".ebook-toc-wrap").height();
+  //   var tocHeight = $(".ebook-toc").height();
 
-    console.log(s);
+  //   var spaceTop = offset.top - 40;
+  //   var spaceBot = tocOffset + tocWrapHeight - tocHeight - 80;
 
-    if ( tocOffset + tocWrapHeight - tocHeight - 80 < s ) {
-      var tocBottomOffset = ($(".ebook-toc").offset()).top;
-      $(".ebook-toc").css({
-        position: "absolute",
-        top: "3721px"
-      })
-    }
-  });
+  //   if ( s > spaceTop && s < spaceBot) {
+  //     $("body").addClass("toc-stick-top");
+  //     $("body").removeClass("toc-stick-bottom");
+  //     $(".ebook-toc").removeAttr("style");
+  //   } else {
+  //     if ( spaceBot < s ) {
+  //       var tocBottomOffset = ($(".ebook-toc").offset()).top;
+  //       $("body").removeClass("toc-stick-top");
+  //       $("body").addClass("toc-stick-bottom");
+  //       $(".ebook-toc").css("top", "3721px")
+  //     }
+  //   }
+  // });
 
 });
