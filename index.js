ac859ae7f263268b410b87aa417061cc1e15b9fa
Daniel O'Connor <daniel@optimizely.com>
2015-07-15 17:26:41 -0700

    Fix the index.js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var shell = require('shelljs');
 
 module.exports = function() {
-  shell.exec('node_modules/.bin/gulp');
+  shell.cd(__dirname);
+  shell.exec('./node_modules/.bin/gulp build assets --marketingCompile --cwd' + __dirname);
 };
