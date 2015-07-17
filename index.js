1fcac4c9ec19bf0a55b173135e3a3b3f69110328
Tom Genoni <tom.genoni@optimizely.com>
2015-07-17 13:24:14 -0700

    Updating gulp call

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var shell = require('shelljs');
 
 module.exports = function() {
   shell.cd(__dirname);
-  shell.exec('./node_modules/.bin/gulp build assets --marketingCompile --cwd' + __dirname);
+  shell.exec('./node_modules/.bin/gulp build sass assets --marketingCompile --cwd' + __dirname);
 };
